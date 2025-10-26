abf42f9fc0d0fe6b9ebe75ab70f6ac0899f27c56
kayoung <pgy8404@naver.com>
2025-10-26 19:17:24 +0900

    Initial project upload

---
+++ src/main/java/org/scoula/ssubom/SsubomApplication.java
@@ -0,0 +1,13 @@
+package org.scoula.ssubom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsubomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsubomApplication.class, args);
+    }
+
+}

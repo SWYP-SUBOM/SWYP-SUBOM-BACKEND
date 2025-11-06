839bcf2b25f6228de4a0e03db9a52928a3b8c785
kayoung <pgy8404@naver.com>
2025-11-06 22:56:44 +0900

    fix :시간 수정

--- src/main/java/swyp_11/ssubom/domain/topic/service/TopicDailyPick.java
+++ src/main/java/swyp_11/ssubom/domain/topic/service/TopicDailyPick.java
@@ -12,7 +12,7 @@ import swyp_11.ssubom.domain.topic.repository.TopicRepository;
 public class TopicDailyPick {
     private final TopicService topicService;
 
-    @Scheduled(cron = "0 53 22 * * *", zone="Asia/Seoul")
+    @Scheduled(cron = "0 0 0 * * *", zone="Asia/Seoul")
     public void dailyPick() {
         for(Long id=1L;id<=5;id++){
             topicService.ensureTodayPicked(id);

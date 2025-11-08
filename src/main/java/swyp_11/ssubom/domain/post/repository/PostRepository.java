c85936ba0fc58abd8580514c7edc3c8aed13d84e
hyeseon <chunghye1998@gmail.com>
2025-11-08 21:48:30 +0900

    feat: 글 발행 시 streak 갱신 로직 구현

--- src/main/java/swyp_11/ssubom/domain/post/repository/PostRepository.java
+++ src/main/java/swyp_11/ssubom/domain/post/repository/PostRepository.java
@@ -3,6 +3,7 @@ package swyp_11.ssubom.domain.post.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import swyp_11.ssubom.domain.post.entity.Post;
+import swyp_11.ssubom.domain.post.entity.PostStatus;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -11,4 +12,6 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findFirstByUser_UserIdAndCreatedAtBetweenOrderByCreatedAtDesc(Long userId, LocalDateTime startOfDay, LocalDateTime endOfDay);
     boolean existsByNickname(String nickname);
+    boolean existsByUser_UserIdAndStatusAndUpdatedAtBetween(Long userId, PostStatus postStatus, LocalDateTime startOfDay, LocalDateTime endOfDay);
+    long countByUser_UserIdAndStatusAndUpdatedAtBetween(Long userId, PostStatus postStatus, LocalDateTime localDateTime, LocalDateTime localDateTime1);
 }

55e6c400baa25ed1fa609bcc3fa8fc39b1d478ac
im0x00 <tkdwns285@gmail.com>
2025-11-10 03:43:05 +0900

    Fix: Correct file case for AiFeedbackRepository

--- src/main/java/swyp_11/ssubom/domain/post/repository/AiFeedbackRepository.java
+++ src/main/java/swyp_11/ssubom/domain/post/repository/AiFeedbackRepository.java
@@ -3,7 +3,6 @@ package swyp_11.ssubom.domain.post.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import swyp_11.ssubom.domain.post.entity.AIFeedback;
-import swyp_11.ssubom.domain.post.entity.Post;
 
 import java.util.Optional;
 

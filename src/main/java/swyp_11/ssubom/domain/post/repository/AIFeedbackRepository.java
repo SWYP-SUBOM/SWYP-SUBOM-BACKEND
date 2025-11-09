2ddcb1d145f067ce60552c12e6b57a793c56779c
hyeseon <chunghye1998@gmail.com>
2025-11-09 21:21:19 +0900

    chore: git file명 인식 문제로 사용하지 않는 AIFeedbackRepository 파일 삭제

--- src/main/java/swyp_11/ssubom/domain/post/repository/AIFeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package swyp_11.ssubom.domain.post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import swyp_11.ssubom.domain.post.entity.AIFeedback;
-
-public interface AIFeedbackRepository extends JpaRepository<AIFeedback, Long> {
-}

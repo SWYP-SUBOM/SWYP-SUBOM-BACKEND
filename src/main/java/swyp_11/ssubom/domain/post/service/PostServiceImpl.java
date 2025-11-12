4658a8944f9ebb0680cdcfd58e367abf9dfddcd6
kayoung <pgy8404@naver.com>
2025-11-12 15:26:45 +0900

    fix: 오늘의 질문 응답구조 수정

--- src/main/java/swyp_11/ssubom/domain/post/service/PostServiceImpl.java
+++ src/main/java/swyp_11/ssubom/domain/post/service/PostServiceImpl.java
@@ -175,9 +175,11 @@ public class PostServiceImpl implements PostService {
         Topic topic = topicRepository.findByUsedAtAndCategory_Id(today,categoryId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.TOPIC_NOT_FOUND));
 
+        Long topicId = topic.getId();
+
         int limit = DEFAULT_PAGE_SIZE;
         List<Post> posts = postRepository.findPostsForInfiniteScroll(
-                categoryId,
+                topicId,
                 cursorUpdatedAt,
                 cursorPostId,
                 limit
@@ -207,7 +209,5 @@ public class PostServiceImpl implements PostService {
 
         return PostListResponseDto.from(topic,postSummaryDtos,nextUpdatedAt, nextPostId, hasMore);
     }
-
-
 
 }

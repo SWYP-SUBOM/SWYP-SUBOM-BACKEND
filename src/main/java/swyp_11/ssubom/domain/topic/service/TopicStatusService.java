fa73fd51aa8dd186b551342d3e781e1e3cf3c5a1
kayoung <pgy8404@naver.com>
2026-01-07 16:06:54 +0900

    fix: 주제 자동생성 에러처리 수정 및 불필요한 log 삭제

---
+++ src/main/java/swyp_11/ssubom/domain/topic/service/TopicStatusService.java
@@ -0,0 +1,30 @@
+package swyp_11.ssubom.domain.topic.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import swyp_11.ssubom.domain.topic.entity.TopicGeneration;
+import swyp_11.ssubom.domain.topic.repository.TopicGenerationRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TopicStatusService {
+    private final TopicGenerationRepository repository;
+
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW) // 새 트랜잭션 강제 생성
+    public void updateToSuccess(Long id) {
+        repository.findById(id).ifPresent(TopicGeneration::complete);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void updateToCompleteWithErrors(Long id, String msg) {
+        repository.findById(id).ifPresent(tg -> tg.completeWithErrors(msg));
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void updateToFail(Long id, String msg) {
+        repository.findById(id).ifPresent(tg -> tg.fail("시스템 오류 : " + msg));
+    }
+}

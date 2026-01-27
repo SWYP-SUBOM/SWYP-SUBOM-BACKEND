48fb22b2ffea8bc7221b8ffdc173dcd627fc9378
hyeseon <chunghye1998@gmail.com>
2026-01-27 21:16:44 +0900

    feat: 인기 글 조회 API 구현

--- src/main/java/swyp_11/ssubom/domain/post/repository/PostRepository.java
+++ src/main/java/swyp_11/ssubom/domain/post/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package swyp_11.ssubom.domain.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swyp_11.ssubom.domain.post.entity.Post;
 import swyp_11.ssubom.domain.post.entity.PostStatus;
@@ -19,4 +21,22 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostRepositor
     List<Post> findByUser_UserIdAndStatusAndCreatedAtBetween(Long userId, PostStatus postStatus, LocalDateTime localDateTime, LocalDateTime localDateTime1);
     List<Post> findByTopicAndStatusOrderByUpdatedAtDesc(Topic topic, PostStatus status);
     long countByUser_UserIdAndStatusAndCreatedAtBetween(Long userId, PostStatus postStatus, LocalDateTime localDateTime, LocalDateTime localDateTime1);
+
+    @Query("""
+    SELECT p.postId
+    FROM Post p
+    LEFT JOIN p.reactions r
+    LEFT JOIN p.postViews pv
+    WHERE p.status = 'PUBLISHED'
+      AND p.createdAt >= :startOfDay
+      AND p.createdAt < :endOfDay
+    GROUP BY p.postId
+    ORDER BY (COUNT(DISTINCT r.id) + COUNT(DISTINCT pv.id)) DESC,
+        p.updatedAt ASC
+    LIMIT 1
+    """)
+    Optional<Long> findTodayPopularPostId(
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay
+    );
 }
